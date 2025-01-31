be324754c3b06fefe8503f5840731e6ff5fab051
Reni-amorim <reni.425@gmail.com>
2025-01-31 13:26:07 -0300

    create filter method

--- app/mapDesconto.js
+++ app/mapDesconto.js
@@ -1,7 +1,7 @@
 function aplicarDesconto(livros) {
     const desconto = 0.2
     livrosDesc = livros.map(livro =>  {
-        return {...livro, preco: livro.preco - (livro.preco * desconto) }
+        return {...livro, preco: parseFloat((livro.preco - (livro.preco * desconto)).toFixed(2))}
     })
     return livrosDesc
 }
